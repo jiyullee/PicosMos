081e5e27fab15b1b0bb7b8909952e815f436b74b
syk_09@naver.com <syk_09@naver.com>
2020-11-06 00:11:03 +0900

    Add photon networkmanager

---
+++ Picosmos/Assets/Scripts/SystemBoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SystemBoot : MonoBehaviour
+{
+
+    /// <summary>
+    /// 씬 로드 완료시에 호출되는 콜백 대리자
+    /// </summary>
+    public Action LevelWasLoaded;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        Application.targetFrameRate = 60;
+        Screen.SetResolution(720, 1280, false);
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        gameObject.AddComponent<GameManager>();
+    
+        SceneLoader.LoadScene(2);
+    }
+
+    
+}
